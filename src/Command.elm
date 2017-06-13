86fdd4852fedc8e7f6c2d4005d511b4d63e7c37f
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-06-13 08:00:50 +0100

    sending post request

--- src/Command.elm
+++ src/Command.elm
@@ -1,7 +1,8 @@
-module Command exposing (..)
+module Command exposing (fetchAstroData)
 
 import Http
 import RemoteData
+import Json.Encode as Encode
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (decode, required, optional)
 import Message
@@ -11,10 +12,19 @@ import Model
 
 fetchAstroData : Cmd Message.Message
 fetchAstroData =
-  Http.get astroUrl astroDataDecoder
+  Http.post astroUrl (Http.jsonBody request) astroDataDecoder
     |> RemoteData.sendRequest
     |> Cmd.map Message.OnFetchAstroData
 
+
+request : Encode.Value
+request = Encode.object
+  [ ("coordinates", Encode.object
+    [ ("latitude", Encode.float 51)
+    , ("longitude", Encode.float 0)
+    ])
+    , ("datetime", Encode.string "2017-05-10T12:12:12.111111+01:00")
+  ]
 
 astroUrl : String
 astroUrl = "https://nzribavfdd.execute-api.us-east-1.amazonaws.com/dev/astro/"
